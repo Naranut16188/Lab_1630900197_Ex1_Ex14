e98e35817b3b7d1a45821a6280fa8abd8b719eab
Naranut16188 <90574866+Naranut16188@users.noreply.github.com>
2021-12-13 23:01:25 +0700

    Add files via upload

---
+++ ex4/js_ex4_2.js
@@ -0,0 +1,5 @@
+//JavaScript by Nawapas Surindr 1630900197
+//Ex4.2: JavaScript Numbers and Strings
+document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
+document.getElementById("ex4-2-2").innerHTML = "Nawapas"; //String
+document.getElementById("ex4-2-3").innerHTML = "Nawapas score: " + 93.69; //Number + String
